ad6f1eed266ea78608be205224d69a12ad54492b
Alexandre Michetti Manduca <a.michetti@gmail.com>
2013-12-04 23:46:57 -0200

    remove the .rb from requires

--- lib/multi_tenancy.rb
+++ lib/multi_tenancy.rb
@@ -1,9 +1,9 @@
-require 'multi_tenancy/tenant.rb'
-require 'multi_tenancy/scope_to_tenant.rb'
+require 'multi_tenancy/tenant'
+require 'multi_tenancy/scope_to_tenant'
 
-require 'multi_tenancy/domain_tenant_resolver.rb'
-require 'multi_tenancy/subdomain_tenant_resolver.rb'
-require 'multi_tenancy/domain_or_subdomain_tenant_resolver.rb'
-require 'multi_tenancy/current_user_tenant_resolver.rb'
+require 'multi_tenancy/domain_tenant_resolver'
+require 'multi_tenancy/subdomain_tenant_resolver'
+require 'multi_tenancy/domain_or_subdomain_tenant_resolver'
+require 'multi_tenancy/current_user_tenant_resolver'
 
-require 'multi_tenancy/scope_to_tenant_filter.rb'
+require 'multi_tenancy/scope_to_tenant_filter'
