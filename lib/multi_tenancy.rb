1cbf8d662c2d6b33e0b254683d6b9d9ad701827d
Alexandre Michetti Manduca <a.michetti@gmail.com>
2013-12-04 23:41:47 -0200

    require lib files

--- lib/multi_tenancy.rb
+++ lib/multi_tenancy.rb
@@ -1,2 +1,9 @@
-module MultiTenancy
-end
+require 'multi_tenancy/tenant.rb'
+require 'multi_tenancy/scope_to_tenant.rb'
+
+require 'multi_tenancy/domain_tenant_resolver.rb'
+require 'multi_tenancy/subdomain_tenant_resolver.rb'
+require 'multi_tenancy/domain_or_subdomain_tenant_resolver.rb'
+require 'multi_tenancy/current_user_tenant_resolver.rb'
+
+require 'multi_tenancy/scope_to_tenant_filter.rb'
